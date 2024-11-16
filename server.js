fdbd90b6e8ea47c6c096e9bfa7f31a561e78a5f2
FGabrielMartins <gabrielreviews8@gmail.com>
2024-11-16 14:31:28 -0300

    mudança de porta

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Usa a porta do Railway ou 3000 como padrão
+const PORT = process.env.PORT || 8080; // Usa a porta do Railway ou 3000 como padrão
 
 // Configurar para servir arquivos estáticos na pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
